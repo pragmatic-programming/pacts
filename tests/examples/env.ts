c0631d5eccaef9acead2b3444248371141e72073
Steven Smyth <ssm@informatik.uni-kiel.de>
2024-05-14 13:54:35 +0200

    Fix example env

--- tests/examples/env.ts
+++ tests/examples/env.ts
@@ -4,9 +4,8 @@ import { Success } from "../../src/UtilityCharts";
 export function env(pac: PragmaticActionChart, callback?: () => void, ticktime: number = 1000) {
 
     setTimeout(() => { 
-        console.log(".");
         pac._tick(callback);
-        if (!pac._terminated) {
+        if (!pac._terminated()) {
             env(pac, callback, ticktime);
         } else {
             console.log("Terminated.");
